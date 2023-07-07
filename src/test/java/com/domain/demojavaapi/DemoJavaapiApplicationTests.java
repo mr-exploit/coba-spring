52b2e42b2d8fe527458c6f448b89985d74415487
mr-exploit <raffiaj8@gmail.com>
2023-07-07 23:16:48 +0700

    belajar spring boot v-1

---
+++ src/test/java/com/domain/demojavaapi/DemoJavaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.demojavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
